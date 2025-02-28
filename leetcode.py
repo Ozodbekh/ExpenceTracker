ccb2d69a6599506c7c60ef67265df0484113b0bb
ozodbekh <ozodbekg800@gmail.com>
2025-02-28 21:41:24 +0500

    nima gap

--- leetcode.py
+++ leetcode.py
@@ -1,5 +1,13 @@
 from typing import List
 
+async def connect_websocket():
+    uri = "ws://10.10.2.133:8000/ws/leaderboard/"
+    async with websockets.connect(uri) as websocket:
+        print("WebSocket-ga ulandi!")
+        while True:
+            message = await websocket.recv()
+            data = json.loads(message)
+            print("Serverdan kelgan ma'lumot:", data)
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
